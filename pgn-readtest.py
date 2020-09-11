7628532edae4f12173ddfbf997a2168b25fcc981
Henry Liu <liuhenry64@gmail.com>
2020-09-11 12:21:17 -0400

    Add test for blunder functionality

--- pgn-readtest.py
+++ pgn-readtest.py
@@ -1,18 +1,36 @@
+"""
+# Henry Liu
+# September 2020
+# pgn-readtest.py
+#
+# Reads in a pgn chess game file and outputs blunders
+# along with the move they were made on
+"""
+
+
 from pgn_parser import parser, pgn
 from stockfish import Stockfish
 import chess
-import chess.engine
+from chess.engine import Cp, Mate, MateGiven
+
+BLUNDER_MARGIN = 75
 
 
 def main():
     board = chess.Board()
     engine = chess.engine.SimpleEngine.popen_uci("/stockfish_10_x64")
-    game = readFile(input("Enter file name: "))
+    # game = readFile(input("Enter file name: "))
+    game = readFile("testGame.pgn")
     analyzeGame(game, board, engine)
 
 
-# FUNCTION: Reads file and formats it into a game object
-# RETURN: A game object 
+
+"""
+# Name: readFile
+# Purpose: Reads file and formats it into a game object
+# Input: the file name
+# Returns: game object
+"""
 def readFile(fileName): 
     try:
         with open(fileName, "r") as file:
@@ -24,14 +42,32 @@ def readFile(fileName):
         print("File \"" + fileName + "\" could not be found")
         exit()
 
+
+"""
+# Name: analyzeGame
+# Purpose: Analyzes game against engine for blunders
+# Input: game object, game board object, chess engine
+# Returns: None
+"""
 def analyzeGame(game, board, engine):
-    m1 = game.move(1)
-    board.push_san(m1.white.san)
-    board.push_san("e5")
-    # board.push_san("Ke2")
-    print(board)
     info = engine.analyse(board, chess.engine.Limit(time=0.1))
-    print("Score:", info["score"])
+    evaluation = info["score"].white().score()
+    # Checks first 20 moves for blunders
+    for x in range(1, 20):
+        # Evaluate position before the moves
+        temp = evaluation
+        # Make the move on the board and evaluate
+        move = game.move(x)
+        board.push_san(move.white.san)
+        board.push_san(move.black.san)
+        info = engine.analyse(board, chess.engine.Limit(time=0.1))
+        evaluation = info["score"].white().score()
+        # Prints score for debugging purposes
+        # print("Score:", info["score"])
+        # Check for blunder
+        if evaluation + BLUNDER_MARGIN < temp :
+            print("Blunder occured on move " + str(x))
+
     engine.quit()
 
 main()
